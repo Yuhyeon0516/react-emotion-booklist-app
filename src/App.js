6b092314ee3a880cb2e78242b4f4114112fc39a7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 18:54:10 +0900

    Route Page 기능 추가 및 Global CSS 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import BookDetailPage from "./pages/BookDetailPage";
+import { Global, css } from "@emotion/react";
+
+const Layout = () => {
+  return (
+    <div>
+      <Global
+        styles={css`
+          body {
+            background-color: white;
+            color: black;
+            transition-duration: 0.2s;
+            transition-property: background-color, color;
+          }
+          a {
+            color: black;
+            text-decoration: none;
+          }
+          ul {
+            list-style: none;
+            padding: 0;
+          }
+        `}
+      />
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SearchPage />} />
+          <Route path="/book/:bookId" element={<BookDetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
